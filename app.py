b8b21b957f11f375f1d994309ec29068a9cc391a
yuhan <linyuhan123@gmail.com>
2025-12-15 13:05:17 +0800

    圖文選單

--- app.py
+++ app.py
@@ -17,7 +17,13 @@ from linebot.v3.messaging import (
     ButtonsTemplate,
     PostbackAction,
     MulticastRequest,
-    PushMessageRequest
+    PushMessageRequest,
+    MessagingApiBlob,
+    RichMenuSize,
+    RichMenuRequest,
+    RichMenuArea,
+    RichMenuBounds,
+    MessageAction
 )
 from linebot.v3.webhooks import (
     MessageEvent,
@@ -25,12 +31,13 @@ from linebot.v3.webhooks import (
     FollowEvent,
     PostbackEvent
 )
-
+import requests
+import json
 from services.json_store import add_transaction
 
 app = Flask(__name__)
-
-configuration = Configuration(access_token='LAU/pl0+Tk9yP0KOr4u4AVE6bAf/xJRGsx8zTCzYj6JwsOjgzdvx964IvNZS6cpCEsxJeR/kaGJDVJsEEd9m6TVZZvotBYbB+8V75nw1alI1CMqYiZgkLRG6lLDk3Wa/IIIQTxPtoQRnhutopzppcQdB04t89/1O/w1cDnyilFU=')
+CHANNEL_ACCESS_TOKEN = 'LAU/pl0+Tk9yP0KOr4u4AVE6bAf/xJRGsx8zTCzYj6JwsOjgzdvx964IvNZS6cpCEsxJeR/kaGJDVJsEEd9m6TVZZvotBYbB+8V75nw1alI1CMqYiZgkLRG6lLDk3Wa/IIIQTxPtoQRnhutopzppcQdB04t89/1O/w1cDnyilFU='
+configuration = Configuration(access_token=CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler('7d9c922a4e31502546357a3109a4d6e4')
 
 
@@ -107,7 +114,76 @@ def handle_message(event):
             )
         )
 
+def create_rich_menu():
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_blob_api = MessagingApiBlob(api_client)
 
+        headers = {
+            'Authorization': 'Bearer ' + CHANNEL_ACCESS_TOKEN,
+            'Content-Type': 'application/json'
+        }
+        body = {
+            "size": {
+                "width": 2500,
+                "height": 843
+            },
+            "selected": True,
+            "name": "圖文選單 1",
+            "chatBarText": "查看更多資訊",
+            "areas": [
+                {
+                "bounds": {
+                    "x": 0,
+                    "y": 0,
+                    "width": 841,
+                    "height": 843
+                },
+                "action": {
+                    "type": "message",
+                    "text": "設定額度"
+                }
+                },
+                {
+                "bounds": {
+                    "x": 836,
+                    "y": 0,
+                    "width": 832,
+                    "height": 843
+                },
+                "action": {
+                    "type": "message",
+                    "text": "本月花費"
+                }
+                },
+                {
+                "bounds": {
+                    "x": 1664,
+                    "y": 0,
+                    "width": 836,
+                    "height": 843
+                },
+                "action": {
+                    "type": "message",
+                    "text": "圖表"
+                }
+                }
+            ]
+        }
+
+        response = requests.post('https://api.line.me/v2/bot/richmenu',headers=headers,data=json.dumps(body).encode('utf-8'))
+        response = response.json()
+        print(response)
+        rich_menu_id = response['richMenuId']
+
+        with open('static/richmenu-1.png', 'rb') as image:
+            line_bot_blob_api.set_rich_menu_image(
+                rich_menu_id = rich_menu_id,
+                body = bytearray(image.read()),
+                _headers = {'Content-Type': 'image/png'}
+            )
+        line_bot_api.set_default_rich_menu(rich_menu_id)
+create_rich_menu()
 '''@handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
     with ApiClient(configuration) as api_client:
